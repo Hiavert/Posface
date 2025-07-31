faacab6dd73b864bd4445a753cfe63c8473698af
Hiavert <ucleshiaverth@gmail.com>
2025-07-31 17:22:40 -0600

    Eliminado Docker del proyecto

--- .docker/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Cambia el puerto 80 por el que te asigna Railway ($PORT)
-sed -i "s/80/${PORT}/g" /etc/apache2/sites-available/000-default.conf
-
-# Arranca Apache en foreground (importante para que el contenedor no se cierre)
-apache2ctl -D FOREGROUND
-
